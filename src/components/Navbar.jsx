33a9a938c31d0bb6139f390524a05d6398ee7bbe
AKDavis95 <112223134+AKDavis95@users.noreply.github.com>
2026-02-18 14:55:45 -0500

    implemented first part of hero section

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { navLinks } from '../../constants/index';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+
+export const Navbar = () => {
+  useGSAP(() => {
+    const navTween = gsap.timeline({
+      scrollTrigger: {
+        trigger: 'nav',
+        // position of the element, viewport position
+        // when the bottom of the nav reaches top of viewport
+        start: 'bottom top',
+      },
+    });
+
+    navTween.fromTo(
+      'nav',
+      { backgroundColor: 'transparent' },
+      {
+        backgroundColor: '#00000050',
+        backgroundFilter: 'blur(10px)',
+        duration: 1,
+        ease: 'power1.inOut',
+      },
+    );
+  });
+
+  return (
+    <nav>
+      <div>
+        <a
+          href="#ome"
+          className="flex items-center gap-2">
+          <img
+            src="/images/logo.png"
+            alt="logo"
+          />
+          <p>Velvet Pour</p>
+        </a>
+        <ul>
+          {navLinks.map((link) => (
+            <li key={link.id}>
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
