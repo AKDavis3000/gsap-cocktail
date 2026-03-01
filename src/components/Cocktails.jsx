91936a0cfd274ca5f4c7ffe5724750932b65e6c9
AKDavis95 <112223134+AKDavis95@users.noreply.github.com>
2026-02-28 22:56:24 -0500

    implemented about section

--- src/components/Cocktails.jsx
+++ src/components/Cocktails.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { cocktailLists, mockTailLists } from '../../constants/index.js';
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
-import { useRef } from 'react';
+// import { useRef } from 'react';
 
 const Cocktails = () => {
   useGSAP(() => {
